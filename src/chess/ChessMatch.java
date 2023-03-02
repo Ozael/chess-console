ad32f077c1cb6637bb1b7bbf3afda1145d39bfcc
Ozael <ozaelvf@gmail.com>
2023-03-02 12:22:07 -0300

    BoardException and defensive programming

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -13,11 +13,11 @@ public class ChessMatch {
         initialSetup();
     }
 
-    public ChessPiece[][] getPieces(){
-        ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getCollumns()];
-        for(int i = 0; i < board.getRows(); i++){
-            for(int j = 0; j < board.getCollumns(); j++){
-                mat[i][j] = (ChessPiece) board.piece(i,j);
+    public ChessPiece[][] getPieces() {
+        ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+        for (int i=0; i<board.getRows(); i++) {
+            for (int j=0; j<board.getColumns(); j++) {
+                mat[i][j] = (ChessPiece) board.piece(i, j);
             }
         }
         return mat;
